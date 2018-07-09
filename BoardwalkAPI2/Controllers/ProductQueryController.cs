11c82fe57f7e6f531de27903f15d10015c9e93cb
Bartek Muszynski <220bartek@gmail.com>
2018-07-09 03:24:01 -0600

    Fix incorrect behavior of Product Query Controller if no search text was given.

--- BoardwalkAPI2/Controllers/ProductQueryController.cs
+++ BoardwalkAPI2/Controllers/ProductQueryController.cs
@@ -13,25 +13,26 @@ namespace BoardwalkAPI2.Controllers {
 
         private const int MAX_ITEMS = 300;
 
-        // GET: /api/ProductQuery/[searchText]
-        // If searchText is missing, interpret the request to get 'Featured' products
+        // GET: /api/ProductQuery/<searchText>
         public IEnumerable<Product> Get(string searchText) {
 
-            IEnumerable<Product> products = null;
+            using (BoardwalkContext context = new BoardwalkContext()) {
+                return context.Products
+                    .Where(x => x.description.Contains(searchText.Trim()))
+                    .Take(MAX_ITEMS)
+                    .ToList();
+            }
+        }
+
+        // GET: /api/ProductQuery
+        // If searchText is missing, interpret the request to get 'Featured' products
+        public IEnumerable<Product> Get() {
 
             using (BoardwalkContext context = new BoardwalkContext()) {
-                if (string.IsNullOrWhiteSpace(searchText))                      // No search-text: return featured products
-                    products = context.Products
-                        .Where(x => x.isfeatured)
-                        .ToList();
-                else                                                            // Return products whose description matches search
-                    products = context.Products
-                        .Where(x => x.description.Contains(searchText.Trim()))
-                        .Take(MAX_ITEMS)
-                        .ToList();
+                return context.Products
+                    .Where(x => x.isfeatured)
+                    .ToList();
             }
-
-            return products;
         }
     }
 }
